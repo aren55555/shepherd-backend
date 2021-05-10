53b61bb2997f77c2922a93104b7755b3cc5b535b
Aren Patel <git@arenpatel.com>
2021-05-09 20:01:40 -0700

    If the server is started with the auth flag:
    go run cmd/main.go --auth=true
    
    The cookie based static auth will be enforced.

---
+++ auth/auth.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"net/http"
+)
+
+func Wrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authCookie, err := r.Cookie("x-auth")
+		if err != nil {
+			http.Error(w, "missing auth cookie!", http.StatusUnauthorized)
+			return
+		}
+		if authCookie.Value != "shepherd" {
+			http.Error(w, "invalid auth cookie!", http.StatusForbidden)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 
 	"github.com/aren55555/shepherd-backend/api"
+	"github.com/aren55555/shepherd-backend/auth"
 	"github.com/aren55555/shepherd-backend/data/mem"
 )
 
 var (
 	port         = flag.Int("port", 8080, "the port for the HTTP server to listen on")
 	fileLocation = flag.String("location", "seed.json", "the location of the seed data JSON file")
+	authEnabled  = flag.Bool("auth", false, "whether the cookie based auth is enabled or not")
 )
 
 func main() {
@@ -24,7 +26,15 @@ func main() {
 	}
 
 	apiHandler := api.New(memDataStore)
-	http.Handle("/api/", apiHandler)
 
+	var h http.Handler = apiHandler
+	if *authEnabled {
+		fmt.Println("AUTH IS ENABLED, ENSURE AN AUTH COOKIE IS SENT!")
+		h = auth.Wrap(apiHandler)
+	}
+
+	http.Handle("/api/v1/", h)
+
+	fmt.Printf("Server started on %d", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
