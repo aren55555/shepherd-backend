68922184fef5176e9cac3faa91ab31115696455b
Aren Patel <git@arenpatel.com>
2021-05-09 20:06:19 -0700

    Cleanup

--- auth/auth.go
+++ auth/auth.go
@@ -4,14 +4,19 @@ import (
 	"net/http"
 )
 
+const (
+	authCookieKey   = "auth"
+	authCookieValue = "shepherd"
+)
+
 func Wrap(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		authCookie, err := r.Cookie("x-auth")
+		authCookie, err := r.Cookie(authCookieKey)
 		if err != nil {
 			http.Error(w, "missing auth cookie!", http.StatusUnauthorized)
 			return
 		}
-		if authCookie.Value != "shepherd" {
+		if authCookie.Value != authCookieValue {
 			http.Error(w, "invalid auth cookie!", http.StatusForbidden)
 			return
 		}
